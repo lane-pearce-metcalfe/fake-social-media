c96f53cb608bfba3f15e2059d8a3df7563e83bd5
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-23 21:50:27 +1200

    Server route for getting user data

---
+++ server/routes/user.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+
+import * as db from '../db/users.ts'
+
+const router = Router()
+
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+
+  try {
+    const user = await db.getUserData(id)
+
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong grabbing posts' })
+  }
+})
+
+export default router
