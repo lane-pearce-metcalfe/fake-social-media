11154d5223360383bad93842ba2d14c9fae5a105
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-23 22:09:44 +1200

    Api function to get a certain users comments

--- client/apis/comments.ts
+++ client/apis/comments.ts
@@ -6,3 +6,8 @@ export async function getPostComments(postId: number) {
   const response = await request.get(`${rootUrl}/post/${postId}`)
   return response.body
 }
+
+export async function getUserComments(userId: number) {
+  const response = await request.get(`${rootUrl}/user/${userId}`)
+  return response.body
+}
