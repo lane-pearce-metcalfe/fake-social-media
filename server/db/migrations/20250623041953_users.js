762d2fe54b89b03716a3033ccab9ad4cbd81c973
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-23 16:22:28 +1200

    Added users table

---
+++ server/db/migrations/20250623041953_users.js
@@ -0,0 +1,15 @@
+export async function up(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('Id')
+    table.string('UserName')
+    table.string('FullName')
+    table.string('Email')
+    table.string('PfpUrl')
+    table.string('UserSince')
+    table.string('Auth0Sub')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('users')
+}
