739b35c5e39b6d5b30d72df88cafdcf9499dd9bc
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-23 21:51:08 +1200

    Fixed error message on getting user data

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -13,7 +13,7 @@ router.get('/:id', async (req, res) => {
     res.json(user)
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong grabbing posts' })
+    res.status(500).json({ message: 'Something went wrong grabbing user data' })
   }
 })
 
