5840a1264c6b17d3bb4761dfea65824fe33e11e6
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-23 17:01:27 +1200

    Database function for grabbing all comments from a certain user

--- server/db/comments.ts
+++ server/db/comments.ts
@@ -3,3 +3,7 @@ import db from './connection.ts'
 export async function getCommentsOnPost(id: number) {
   return await db('comments').where({ PostId: id }).select()
 }
+
+export async function getCommentsFromUser(id: number) {
+  return await db('comments').where({ UserId: id }).select()
+}
