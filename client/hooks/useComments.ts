e85507fff8dda39c515d6c20cb44593bc2aa8318
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-24 12:22:28 +1200

    Hook function to get a users comments

--- client/hooks/useComments.ts
+++ client/hooks/useComments.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
-import { getPostComments } from '../apis/comments'
+import { getPostComments, getUserComments } from '../apis/comments'
 
 export function usePostComments(postId: number) {
   const query = useQuery({
     queryKey: ['postComments', postId],
     queryFn: () => getPostComments(postId),
+  })
+  return query
+}
+
+export function useUserComments(userId: number) {
+  const query = useQuery({
+    queryKey: ['userComments', userId],
+    queryFn: () => getUserComments(userId),
   })
   return query
 }
